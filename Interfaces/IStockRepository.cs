fe3fe573b2e44a62daa105f0e7077d1de7d737e3
M0o4 <indrei457@gmail.com>
2024-06-08 15:57:25 +0300

    chore(Interfaces): create IStockRepository interface

---
+++ Interfaces/IStockRepository.cs
@@ -0,0 +1,14 @@
+using backend.Dtos.Stock;
+using backend.Models;
+
+namespace backend.Interfaces
+{
+    public interface IStockRepository
+    {
+        Task<List<Stock>> GetAllStocksAsync();
+        Task<Stock?> GetByIdAsync(int id);
+        Task<Stock> CreateAsync(Stock stockModel);
+        Task<Stock?> UpdateStockAsync(int id, UpdateStockRequestDto stockModel);
+        Task<Stock?> DeleteAsync(int id);
+    }
+}
